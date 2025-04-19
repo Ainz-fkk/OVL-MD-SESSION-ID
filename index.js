2cc98aa49c3766ca1849de66bec31e4fd7b02ba8
Ainz-fkk <ainzgit@outlook.fr>
2025-04-19 15:31:36 +0000

    Create index.js

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = process.env.PORT || 8000;
+let code = require('../JS/pair');
+const router = require('../JS/qr');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/code', code);
+app.use('/qr', router);
+
+app.use('/pair', (req, res) => {
+    res.sendFile(path.join(__dirname, 'pair.html'));
+});
+
+app.use('/qrcode', (req, res) => {
+    res.sendFile(path.join(__dirname, 'qr.html'));
+});
+
+app.use('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'main.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Serveur en cours d'exécution sur http://localhost:${PORT}`);
+});
+
+module.exports = app;
